e321fc6789e046e95f1e53f28a01ed447b7bbc98
GameFreak1337 <167435606+GameFreak1337@users.noreply.github.com>
2024-04-18 10:44:52 -0400

    Add files via upload

---
+++ jest.config.js
@@ -0,0 +1,23 @@
+module.exports = {
+  roots: ["<rootDir>/src/", "<rootDir>/test/"],
+  moduleFileExtensions: ["ts", "tsx", "js", "jsx"],
+  transform: {
+    "^.+\\.(js|jsx|ts|tsx)$": "babel-jest",
+  },
+  transformIgnorePatterns: ["node_modules/(?!react-markdown)/"],
+  testPathIgnorePatterns: [".cypress", "node_modules", "dist"],
+  testEnvironment: "./FixJSDOMEnvironment.ts",
+  setupFiles: ["./jest.polyfills.js"],
+  moduleNameMapper: {
+    "\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$":
+      "<rootDir>/test/__mocks__/fileMock.js",
+    "\\.(css|less)$": "<rootDir>/test/__mocks__/NullMock.js",
+    "\\!!raw-loader!.*$": "<rootDir>/test/__mocks__/NullMock.js",
+    "@player": "<rootDir>/src/Player",
+    "@enums": "<rootDir>/src/Enums",
+    "@nsdefs": "<rootDir>/src/ScriptEditor/NetscriptDefinitions",
+    "^monaco-editor$": "<rootDir>/test/__mocks__/NullMock.js",
+    "^monaco-vim$": "<rootDir>/test/__mocks__/NullMock.js",
+    "/utils/Protections$": "<rootDir>/test/__mocks__/NullMock.js",
+  },
+};
